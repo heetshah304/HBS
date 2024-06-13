1f78f086e35366f59b281ca82c37990851466e9c
Heet Shah <heetshah.sb@gmail.com>
2024-06-12 21:44:49 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const customHelpers = require("./views/helpers/customHelpers")
+
+app.engine(
+    ".hbs",
+    exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
+    helpers: customHelpers
+    })
+    );
+app.set("view engine", ".hbs");
+// replaced information
+app.get("/", (req, res) => {
+    // find the home.hbs file, and fill in the information
+    res.render("home", {
+    title: "Home Page",
+    message: "Welcome to Handlebars with Express!",
+    });
+    });
+
+    // if
+app.get("/if", (req, res) => {
+res.render("if", { user: sampleData.user });
+});
+// unless
+app.get("/unless", (req, res) => {
+res.render("unless", { condition: sampleData.condition });
+});
+// each
+app.get("/each", (req, res) => {
+res.render("each", { users: sampleData.users });
+});
+
+//Sample data
+const sampleData = {
+user: { name: "John Doe", email: "john@example.com" },
+users: [
+{ name: "Alice", email: "alice@example.com" },
+{ name: "Bob", email: "bob@example.com" },
+],
+condition:false,
+};
+
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+console.log(`http://localhost:${PORT}`);
+});
